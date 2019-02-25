2d9ba14043794fe6281863350fe4303ae3b98d10
Samuel W Tong <ssamt1142@gmail.com>
2019-02-25 03:10:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,9 @@
 require"reverse_each_word_spec.rb"
 def reverse_each_word(sentence)
-  
   split_str = sentence.split(" ")
   reverse = []
   split_str.each do |n|
     reverse << n.reverse!
   end
   reverse.join(" ")
-
-  reverse_2 = split_str.collect do |x|
-    x.reverse!
-  end
-  reverse_2.join(" ")
 end
